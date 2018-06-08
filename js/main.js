f537743a6a12013261421feb9902acd21a5d29e1
gifton <giftono@gmail.com>
2018-06-08 04:47:12 -0700

    fixed everything i broke

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
+function show_projects() {
+    document.getElementById('about').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+    document.getElementById('projects').style.display = 'block';
+    document.getElementById('project-btn').setAttribute('color', 'green');
+    document.getElementById('contacts-btn').setAttribute('color', 'black');
+    document.getElementById('about-btn').setAttribute('color', 'black');
+}
+
 function show_about() {
     document.getElementById('projects').style.display = 'none';
     document.getElementById('contact').style.display = 'none';
     document.getElementById('about').style.display = 'block';
+    document.getElementById('about-btn').setAttribute('color', 'green');
+    document.getElementById('contacts-btn').setAttribute('color', 'black');
+    document.getElementById('project-btn').setAttribute('color', 'black');
 }
 
 function show_contact() {
     document.getElementById('projects').style.display = 'none';
     document.getElementById('about').style.display = 'none';
     document.getElementById('contact').style.display = 'block';
-}
-
-function show_projects() {
-    document.getElementById('about').style.display = 'none';
-    document.getElementById('contact').style.display = 'none';
-    document.getElementById('projects').style.display = 'block';
+    document.getElementById('contact-btn').setAttribute('color', 'green');
+    document.getElementById('project-btn').setAttribute('color', 'black');
+    document.getElementById('about-btn').setAttribute('color', 'black');
 }
 
 function scroll_down() {
