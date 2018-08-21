e8a462572c9e677fb0319c7ffb40a01409fce27f
gifton <giftono@gmail.com>
2018-08-20 18:34:37 -0700

    updated design, WIP

--- js/main.js
+++ js/main.js
@@ -1,30 +1,8 @@
-function show_projects() {
-    document.getElementById('about').style.display = 'none';
-    document.getElementById('contact').style.display = 'none';
-    document.getElementById('projects').style.display = 'block';
-    document.getElementById('project-btn').setAttribute('color', 'green');
-    document.getElementById('contacts-btn').setAttribute('color', 'black');
-    document.getElementById('about-btn').setAttribute('color', 'black');
-}
-
-function show_about() {
-    document.getElementById('projects').style.display = 'none';
-    document.getElementById('contact').style.display = 'none';
-    document.getElementById('about').style.display = 'block';
-    document.getElementById('about-btn').setAttribute('color', 'green');
-    document.getElementById('contacts-btn').setAttribute('color', 'black');
-    document.getElementById('project-btn').setAttribute('color', 'black');
-}
-
-function show_contact() {
-    document.getElementById('projects').style.display = 'none';
-    document.getElementById('about').style.display = 'none';
-    document.getElementById('contact').style.display = 'block';
-    document.getElementById('contact-btn').setAttribute('color', 'green');
-    document.getElementById('project-btn').setAttribute('color', 'black');
-    document.getElementById('about-btn').setAttribute('color', 'black');
-}
-
-function scroll_down() {
-    $('html, body').animate({scrollTop:$(document).height()}, 'slow');
-}
+$(window).load(function() {
+  $('.post-module').hover(function() {
+    $(this).find('.description').stop().animate({
+      height: "toggle",
+      opacity: "toggle"
+    }, 300);
+  });
+});
